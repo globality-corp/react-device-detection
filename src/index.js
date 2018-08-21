fecfff11b33cd05caa1ba4ebba159e90ae4f341a
Jaime Oyarzun K <jaime.oyarzun@globality.com>
2018-08-21 12:42:51 -0700

    Refactor HOC

--- src/index.js
+++ src/index.js
@@ -19,47 +19,34 @@ import { deviceTypes, MOBILE_DEVICE, CHROME_BROWSER, FIREFOX_BROWSER, IE_BROWSER
 import { deviceToPropMap, evaluate, Singleton } from './utils/index';
 
 export default function reactDeviceDetection (...config) {
-    return WrappedComponent => class extends React.Component {
+    return (WrappedComponent) => {
+        const uaParser = Singleton.instance(UAParser);
+        const deviceData = uaParser.getDevice();
+        const browserData = uaParser.getBrowser();
 
-        constructor () {
-            super();
-            
-            const uaParser = Singleton.instance(UAParser);
-            const deviceData = uaParser.getDevice();
-            const browserData = uaParser.getBrowser();
-
-            this.state = {
-                isChrome: false,
-                isIE: false,
-                isMobile: false,
-                isFirefox: false,
+        return class extends React.Component {
+            state = {
                 config,
-                ...this.populateData({ deviceData, browserData })
-            };
-        }
-        
-        populateData ({ deviceData, browserData }) {
-            return {
                 isChrome: evaluate(browserData, CHROME_BROWSER),
                 isIE: evaluate(browserData, IE_BROWSER),
                 isMobile: evaluate(deviceData, MOBILE_DEVICE, 'type'),
                 isFirefox: evaluate(browserData, FIREFOX_BROWSER),
             };
-        }
 
-        getInjections () {
-            return this.state.config
-                .filter(arg => deviceTypes.some(type => arg === type))
-                .map(device => ({
-                    [deviceToPropMap.get(device)]: this.state[deviceToPropMap.get(device)],
-                }))
-                .reduce((acc, curr) => ({ ...acc, ...curr }), {});
-        }
+            getInjections () {
+                return this.state.config
+                    .filter(arg => deviceTypes.some(type => arg === type))
+                    .map(device => ({
+                        [deviceToPropMap.get(device)]: this.state[deviceToPropMap.get(device)],
+                    }))
+                    .reduce((acc, curr) => ({ ...acc, ...curr }), {});
+            }
 
-        render () {
-            const injections = this.getInjections();
-            return <WrappedComponent {...this.props} {...injections} />;
-        }
+            render () {
+                const injections = this.getInjections();
+                return <WrappedComponent {...this.props} {...injections} />;
+            }
+        };
     };
 }
 
